c32fd61d8876d5c3acedcf990fef0fb6c8d2b392
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-07 11:57:05 +0000

    Add template for component

---
+++ src/components/template/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import classes from './Navbar.module.css'
+
+function Text(props) {
+  return <h1>template</h1>
+}
+
+Text.propTypes = {}
+
+export default Text
