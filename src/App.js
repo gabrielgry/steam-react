ff944fe57670ad06aa0acfb2f8071210afe91c53
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-07 12:00:42 +0000

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import NavbarItem from './components/NavbarItem'
+import Appbar from './components/Appbar'
+import IconButton from './components/IconButton'
+import {
+  MdGames,
+  MdOutlineGames,
+  MdEmojiEvents,
+  MdOutlineEmojiEvents,
+  MdGroups,
+  MdOutlineGroups,
+  MdFeed,
+  MdOutlineFeed,
+} from 'react-icons/md'
 
 function App() {
+  const [state, setState] = useState(0)
+
+  const handleChange = (event, value) => {
+    setState(value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Appbar>
+        <IconButton>
+          <MdGames />
+        </IconButton>
+      </Appbar>
+      <Navbar value={state} onChange={(event, value) => handleChange(event, value)}>
+        <NavbarItem icon={<MdOutlineGames />} iconActive={<MdGames />}>
+          Games
+        </NavbarItem>
+        <NavbarItem icon={<MdOutlineEmojiEvents />} iconActive={<MdEmojiEvents />}>
+          Archivements
+        </NavbarItem>
+        <NavbarItem icon={<MdOutlineGroups />} iconActive={<MdGroups />}>
+          Friends
+        </NavbarItem>
+        <NavbarItem icon={<MdOutlineFeed />} iconActive={<MdFeed />}>
+          News
+        </NavbarItem>
+      </Navbar>
+    </>
+  )
 }
 
-export default App;
+export default App
