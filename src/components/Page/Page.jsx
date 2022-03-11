b2349400e279eaf1ee3aed425d2322272beeac5a
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-11 19:44:52 +0000

    Add Page component

---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+import Surface from '../Surface'
+
+const Spacer = styled(Surface)`
+  height: 64px;
+  width: 100%;
+`
+
+const Root = styled(Surface)`
+  width: 100%;
+  min-height: ${({ fullHeight }) => (fullHeight ? '100vh' : 'calc(100vh - 64px)')};
+  padding: 16px;
+`
+
+function Page(props) {
+  const { spacer, fullHeight, children, ...rootProps } = props
+
+  return (
+    <>
+      {spacer && <Spacer surfaceColor={'background'} />}
+      <Root
+        component='main'
+        surfaceColor={'background'}
+        contentColor={'onBackground'}
+        fullHeight={fullHeight}
+        {...rootProps}
+      >
+        {children}
+      </Root>
+    </>
+  )
+}
+
+Page.propTypes = {
+  children: PropTypes.node,
+  spacer: PropTypes.bool,
+  fullHeight: PropTypes.bool,
+  surfaceColor: PropTypes.string,
+  contentColor: PropTypes.string,
+}
+
+export default Page
