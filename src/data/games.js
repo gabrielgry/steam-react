07f7e1351826ae972343e2181e3808f815e4c90a
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-11 19:44:24 +0000

    Add game data file

---
+++ src/data/games.js
@@ -0,0 +1,47 @@
+const data = {
+  lastPlayed: ['1245620', '1593500', '892970'],
+  games: [
+    {
+      name: 'Half-Life 2',
+      id: '220',
+    },
+    {
+      name: 'Portal',
+      id: '400',
+    },
+    {
+      name: 'Portal 2',
+      id: '620',
+    },
+    {
+      name: 'Limbo',
+      id: '48000',
+    },
+    {
+      name: 'Life is Strange',
+      id: '319630',
+    },
+    {
+      name: 'Dark Souls 3',
+      id: '374320',
+    },
+    {
+      name: 'Valheim',
+      id: '892970',
+    },
+    {
+      name: 'Apex Legends',
+      id: '1172470',
+    },
+    {
+      name: 'Elden Ring',
+      id: '1245620',
+    },
+    {
+      name: 'God of War',
+      id: '1593500',
+    },
+  ],
+}
+
+export default data
