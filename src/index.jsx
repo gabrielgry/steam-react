90d208999db2fe9c4e45f2ec2d23791f5f95d942
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-11 19:39:05 +0000

    Update index entrypoint file

---
+++ src/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+import { ThemeProvider, createGlobalStyle } from 'styled-components/macro'
+import './assets/css/normalize.css'
+import theme from './theme'
+import App from './App'
+
+const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    font-family: Roboto, 'Noto Sans', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+  html, body, #root {
+    width: 100%;
+    min-height: 100vh;
+  }
+}
+`
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
