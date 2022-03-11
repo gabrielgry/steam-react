a91b1effe0034b5b23046dd21fd21c249cbe329b
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-11 19:40:35 +0000

    Update component template

---
+++ src/components/Template/Template.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components/macro'
+
+const Root = styled.div``
+
+function Template(props) {
+  return <Root></Root>
+}
+
+Template.defaultProps = {}
+
+Template.propTypes = {}
+
+export default Template
