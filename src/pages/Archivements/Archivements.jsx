ca516c980d75e9f37e73df048db24dc4c2ddfb02
Gabriel Gryuk <gabrielgrysuk@gmail.com>
2022-03-15 16:39:38 +0000

    Add Archivements page

---
+++ src/pages/Archivements/Archivements.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+// import styled from 'styled-components/macro'
+import Page from '../../components/Page'
+import Text from '../../components/Text'
+
+function Archivements(props) {
+  return (
+    <Page navbar>
+      <Text type='headline-small'>Archivements</Text>
+    </Page>
+  )
+}
+
+Archivements.defaultProps = {}
+
+Archivements.propTypes = {}
+
+export default Archivements
